0ecf6413cf0ac74c91d46de33ebbbde34b0a24a0
rizramadan <rizki123304@gmail.com>
2020-12-22 22:50:29 +0700

    progres

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'splashscreen.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'SplashScreen',
+    home: SplashScreenPage(),
+  ));
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'form.dart';
+import 'dart:async';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return Login();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffffffff),
+        body: Center(
+          child: Image.asset(
+            "lib/images/logos.JPG",
+            width: 500.0,
+            height: 300.0,
+          ),
+        ));
+  }
+}
